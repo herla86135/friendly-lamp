bbdef0c2eccae86d341da7de210479c70d4725cc
a <a@b.c>
2018-04-12 19:57:47 +0530

    update

--- url.js
+++ url.js
@@ -1,1 +1,1 @@
-{"_repo_url": "https://cdn.rawgit.com/herla86135/friendly-lamp/e7d3be826adf7d39eb74ca23afe7cc8d5d9666da/script.js"}
+{"url": "https://cdn.rawgit.com/herla86135/friendly-lamp/e7d3be826adf7d39eb74ca23afe7cc8d5d9666da/script.js"}
