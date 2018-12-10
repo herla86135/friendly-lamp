4a58406d476751aa8a238717c879f7c34c28019c
herla86135 <38293164+herla86135@users.noreply.github.com>
2018-12-10 20:25:09 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,1 +1,1 @@
-{"url": "https://cdn.jsdelivr.net/gh/herla86135/friendly-lamp@3e7381c5913a256d3fd55708decf89bcb62d911e/script.js"}
+{"url": "https://cdn.jsdelivr.net/gh/herla86135/friendly-lamp@3e7381c5913a256d3fd55708decf89bcb62d911e/script.min.js"}
